75a784672bb729f55ee6e9012fe100e93b795554
Rannyeri Baptist <rannyerirbatista@gmail.com>
2020-08-11 23:24:06 -0300

    about page. font in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "cities#index"
+
+  get "/about", to: "application#about", as: :about_page
 end
